54168be6457dc318e8fca53084646b6b06e3f7ad
Janet D <74864674+anise-design@users.noreply.github.com>
2020-11-23 17:51:20 +0000

    Add files via upload

--- js/project3.js
+++ js/project3.js
@@ -1,19 +1,21 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Your code here...
+  
 
   // SITEMAP js
 
 
-  // Checking user actions for SEARCH box
-  var clickSearch = document.getElementById('searching'); //button
+  // Using Google.com for on-site SEARCH box
+  // Code from https://pagedart.com/blog/how-to-add-a-search-bar-in-html/
+  
   var f = document.getElementById('gform');  //form
   var q = document.getElementById('search');  //text entered
-//const q = document.getElementById('query');  
+
 
   const google = 'https://www.google.com/search?q=site%3A+';
-  const site = 'anise-design.github.io';
+  const site = 'anise-design.github.io'; 
+  //our website which is currently being indexed by Google
 
   function submitted(event) {
     event.preventDefault();
@@ -23,31 +25,12 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   f.addEventListener('submit', submitted);
-  
-  /*
-  clickSearch.addEventListener('click', function(ev) {
-    // When button is clicked...
-    if (q && q.value) {
-      // and text has been entered
-      alert("Search in progress."); 
-    } else {
-      // Tell user no search text entered
-      alert("No search entered");
-    }    
-  }); 
-  */
+ 
 
 
-  // Alert box to indicate when email is entered
 
-/*
-  var textInput = document.getElementById('email');
-  textInput.addEventListener('input', function(e) {
-      alert("Email addressed entered.\nClick the 'Subscribe' button to complete.");
-  });
-  */
+  // Alert box to feedback on user action for NEWSLETTER SUBSCRIBE
 
-  // Another alert box to indicate when 'Subscribe' button is clicked
   var clickButton = document.getElementById('button'); //button
   var subscribeText = document.getElementById('email'); //text entered
 
@@ -55,10 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
   clickButton.addEventListener('click', function(event) {
 
     if (subscribeText && subscribeText.value) {
-      alert("Thank you for subscribing");
+      alert("Thank you for subscribing.");
     } else {
       // Tell user no search text entered
-      alert("No email entered");
+      alert("No email entered. Try again.");
     }
        
   });
